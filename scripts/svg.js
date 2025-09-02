bac537e936c5cb205c62250d2c4a570a25010708
hannahliu123 <hannah.liu2010@gmail.com>
2025-09-01 21:32:53 -0400

    page format edit
    uh we're gonna have to change to dragging to just plain js :(

--- scripts/svg.js
+++ scripts/svg.js
@@ -1,34 +1,35 @@
 const container = document.getElementById("sheet-container");
-const width = container.clientWidth;
+const width = container.clientWidth - 100;
 const height = 11*width/8.5;    // based on average paper sizes
-var draw = SVG().addTo(container).size(width, height)
-// set width & height dimensions of canvas to based on container
 
-const pageWidth = width - 0.05*window.innerWidth;
-const pageHeight = 11*pageWidth/8.5;
-var page = draw.rect(pageWidth, pageHeight)
-    .fill("white")
-    .move(0.025*window.innerWidth, 0.025*window.innerWidth);
+function addPage() {
+    const svg = SVG().addTo(container).size(width, height);
+    svg.rect(width, height).fill("white");
+    return svg;
+}
 
-var text = draw.text("hello")
+const page = addPage();
+// const page2 = addPage();
+
+var text = page.text("hello")
     .font({
         size: "1.25rem",    // font-size (number means pixels, use quotes if using units)
         anchor: "middle",   // text alignment
         family: "Arial"
-    }).move(100, 60);       // x, y
+    })
+    .move(100, 60)      // x, y
+    .draggable().on("dragmove.namespace", (event) => {
+        const { handler, box } = event.detail;
+        event.preventDefault();
+        handler.move(box.x - (box.x % 10), box.y - (box.y % 100));
+    });
 
-text.draggable().on("dragmove.namespace", (event) => {
-    const { handler, box } = event.detail;
-    event.preventDefault();
+const baseNote = page.group();
+baseNote.add(page.text("1")).font({ size:"1.25rem" });
+baseNote.add(page.circle(5).fill("black").move(4, 0));
 
-    handler.move(box.x - (box.x % 10), box.y - (box.y % 100));
-});
+var note1 = baseNote.clone().draggable().addTo(page);
+var note2 = baseNote.clone().draggable().addTo(page);
 
-const note = draw.group();
-note.add(draw.text("1")).font({ size:"1.25rem" });
-note.add(draw.circle(5).fill('black').move(4, 0));
-note.move(200, 200);
-note.draggable();
-
-var svgData = draw.svg();   // XML
+var svgData = page.svg();   // XML
 console.log(svgData);
